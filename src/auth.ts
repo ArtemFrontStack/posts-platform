e1951703124f4d6bac518b7c0a4e1ba28f1a276e
Artem-CyberArt <artem2006pax@mail.ru>
2025-05-05 14:50:37 +0300

    change project

--- src/auth.ts
+++ src/auth.ts
@@ -2,8 +2,16 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Yandex from "next-auth/providers/yandex";
 
-export const { handlers , signIn, signOut , auth} = NextAuth(
-    {
-        providers: [Google, Yandex],
-    }
-)
+export const { handlers, auth, signIn } = NextAuth({
+    providers: [
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID as string,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+        }),
+        Yandex({
+            clientId: process.env.AUTH_YANDEX_ID as string,
+            clientSecret: process.env.AUTH_YANDEX_SECRET as string,
+        }),
+    ],
+    secret: process.env.AUTH_SECRET, // Ключ для подписи сессий
+});
